2e728b8cdcac379d4b7ed8a991597fe70a70e0b9
Subedat Adeyemi <subedatade@gmail.com>
2023-10-31 06:01:30 -0700

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#Author - Subedat Adeyemi
+
+"""Print numbers ) to (8 in decimal and hexadecimal"""
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
